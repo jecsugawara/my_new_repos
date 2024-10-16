ee9204e81c51bde8e522cd43cb2dddadee3e1f6d
jecsugawara <sugawara@jec.ac.jp>
2024-10-16 15:26:32 +0900

    日本電子専門学校の1行を追加

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
   public static void main(String[] args){
     System.out.println("HelloWorld");
     System.out.println("HelloWorld");
-    System.out.println("HelloWorld");
+    System.out.println("日本電子専門学校");
   }
 }
