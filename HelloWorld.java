1632545b07ffbc6806e413b939c9421592470854
jecsugawara <sugawara@jec.ac.jp>
2024-10-23 16:20:44 +0900

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,7 @@
 public class HelloWorld {
   public static void main(String[] args){
-    System.out.println("HelloWorld");
-    System.out.println("HelloWorld");
-    System.out.println("日本電子専門学校");
-    System.out.println("日本電子専門学校");
-    System.out.println("日本電子専門学校");
-    System.out.println("日本電子専門学校");
-    System.out.println("日本電子専門学校");
-    System.out.println("日本電子専門学校");
-    
+    for(int i=0 ; i<10; i++){
+        System.out.println("HelloWorld");
+    }
   }
 }
-++
